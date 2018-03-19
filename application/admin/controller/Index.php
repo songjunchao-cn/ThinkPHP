02063b591fda23771888f3245807e713b66eb344
Nofiction <songjunchao.cn@gmail.com>
2018-03-19 15:44:38 +0800

    提交tp5

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\admin\controller;
+class Index{
+public function index()
+  {
+    return "我是后台主页控制器";
+    # code...
+  }
+}
+ ?>
---
+++ application/index/controller/User.php
@@ -0,0 +1,8 @@
+<?php
+namespace app\index\controller;
+class User{
+public function user(){
+  return "我是user方法";
+}
+}
+ ?>
